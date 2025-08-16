3907c019391ec958486fbde7a08c73cd846052c2
voreff <yakidza05@gmail.com>
2025-08-16 12:53:18 +0300

    Updated trace

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/9a4ee768fed045da-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
